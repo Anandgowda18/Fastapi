0b7e75ba3bf7054e78e841514261dc8c104c5cb5
Anand M A <happyanand849@gmail.com>
2026-03-13 07:22:03 +0530

    added  pydantic schema validation

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+from typing import Optional
+
 from fastapi import FastAPI
 from fastapi.param_functions import Body
+from pydantic import BaseModel
 from typing_extensions import Dict
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    """This is class which defines the basemodel using pydantic to validate the input schema that the user should send"""
+
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 
 @app.get("/")
@@ -19,3 +31,11 @@ async def get_posts():
 async def create_posts(payload: Dict = Body(...)):
     """payload is the variable of type dict, Body is a special function used to declare and validate request body parameters"""
     return {"payload": payload}
+
+
+@app.post("/createpostsval")
+async def create_posts_val(new_post: Post):
+    """new_post is a variable, Post is from the class Post to validate the pydantic schema to check if the user is sending all the data in a type we expect"""
+    print(new_post)
+    new_post_dict = new_post.dict()
+    return {"Valid": "Data upload is valid", "uploaded data is": new_post_dict}
